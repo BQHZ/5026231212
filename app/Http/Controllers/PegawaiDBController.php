3d59eb8ae2110796c1240fa032b4e3dba7180f48
BQHZ <baqhizzz@gmail.com>
2025-06-04 10:11:18 +0700

    Pertemuan 14

--- app/Http/Controllers/PegawaiDBController.php
+++ app/Http/Controllers/PegawaiDBController.php
@@ -9,7 +9,8 @@ class PegawaiDBController extends Controller
     public function index()
     {
     	// mengambil data dari table pegawai
-    	$pegawai = DB::table('pegawai')->get();
+    	// $pegawai = DB::table('pegawai')->get();
+        $pegawai = DB::table('pegawai')->paginate(15);
 
     	// mengirim data pegawai ke view index
     	return view('index',['pegawai' => $pegawai]);
@@ -72,4 +73,19 @@ class PegawaiDBController extends Controller
         // alihkan halaman ke halaman pegawai
         return redirect('/pegawai');
     }
+
+    public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$pegawai = DB::table('pegawai')
+		->where('pegawai_nama','like',"%".$cari."%")
+		->paginate();
+
+    		// mengirim data pegawai ke view index
+		return view('index',['pegawai' => $pegawai]);
+
+	}
 }
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,20 +1,67 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('template')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+@section('content')
 	<h3>Data Pegawai</h3>
 
-	<a href="/pegawai"> Kembali</a>
+	<a href="/pegawai class=btn btn-info"> Kembali</a>
 
 	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
+            {{-- CSRF Token --}}
 		{{ csrf_field() }}
+            <div class="form-group has-success">
+                <label class="control-label col-sm-2" for="id1" for="nama">
+                    Nama
+                </label>
+                <div class="col-sm-6">
+                    <input class="form-control"
+                           type="text"
+                           id="nama"
+                           name="nama"
+                           placeholder="Masukan Nama Lengkap"
+                           required="required">
+                </div>
+            </div>
+            <div class="form-group has-success">
+                <label class="control-label col-2" for="Jabatan">
+                    Jabatan
+                </label>
+                <div class="col-sm-6">
+                    <input class="form-control"
+                           type="text"
+                           id="jabatan"
+                           name="jabatan"
+                           placeholder="Masukan Jabatan saat ini"
+                           required="required">
+                </div>
+            </div>
+            <div class="form-group has-success">
+                <label class="control-label col-2" for="umur">
+                    Umur
+                </label>
+                <div class="col-sm-6">
+                    <input class="form-control"
+                           type="text"
+                           id="umur"
+                           name="umur"
+                           placeholder="Masukan Umur"
+                           required="required">
+                </div>
+            </div>
+<div class="form-group">
+            <label class="control-label col-sm-2" for="alamat">
+                Alamat
+            </label>
+            <div class="col-6">
+                   <textarea class="form-control"
+                   type="text"
+                   id="alamat"
+                   placeholder="Masukkan Alamat Lengkap"
+                   name="alamat" required="required"></textarea>
+                </div>
+        </div>
 		Nama <input type="text" name="nama" required="required"> <br/>
 		Jabatan <input type="text" name="jabatan" required="required"> <br/>
 		Umur <input type="number" name="umur" required="required"> <br/>
